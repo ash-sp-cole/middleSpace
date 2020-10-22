453381cbd3b690630e049e0dd4dda423f017c9c8
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2020-10-22 13:09:12 +0200

    update build

--- server.js
+++ server.js
@@ -14,10 +14,11 @@ const PORT = 3001 || process.env.PORT;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-
- app.use(express.static("client/build"));
-    
+if (process.env.NODE_ENV === "production") {
+        app.use(express.static("client/build"));
+      }
+      
+      
  app.get('/testlive', function (req, res) {
         res.send('GET request to the homepage')
       })
